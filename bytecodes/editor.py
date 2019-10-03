ac5c1df597c5b85d886470cadc387fc18d4f1774
jsyoon <decinc.kr@gmail.com>
2019-10-03 18:07:13 +0900

    add label

---
+++ bytecodes/editor.py
@@ -0,0 +1,17 @@
+
+class Editor(object):
+  def __init__(self, code_item):
+    self.tries = []
+    self.opcodes = []
+
+  def commit(self):
+    pass
+
+  def get_opcode_list(self):
+    pass
+
+  def is_in_try(self, code):
+    pass
+
+  def get_opcode_offset(self, opcode):
+    pass
