e531d2904e5c6d6952ec27b0f29ad6a72639ee7a
KimCom903 <55732371+KimCom903@users.noreply.github.com>
2019-10-25 15:48:10 +0900

    Update dex.py

--- dex/dex.py
+++ dex/dex.py
@@ -674,9 +674,11 @@ class CodeItem(DexItem):
       self.handlers = EncodedCatchHandlerList(self.manager, self.root_stream, self.base_index + self.read_size)
       self.read_size += self.handlers.read_size
       print('finished, catch_handler_list')
+      #이 부분을 읽어 올떄 잘못 되는거 같아요 hnadlerList_size가 2이상일 떄만 가끔 값이 이상해집니다.
     for x in self.tries:
       x.handlers = EncodedCatchHandler(self.manager, self.root_stream, x.handler_off + self.handlers.base_index)
     #print('parse finished')
+    #이부분을 돌때에는 대부분 정상적인 값들이 나옵니다.
 class TryItem(DexItem):
   descriptor = {
     'start_addr': UINT,
@@ -698,6 +700,10 @@ class EncodedCatchHandlerList(DexItem):
       item = EncodedCatchHandler(self.manager, self.root_stream, self.base_index + self.read_size)
       self.list.append(item)
       self.read_size += item.read_size
+    #그래서 여기서 read_size업데이트가 잘못되는거 아닌가 생각됩니다.
+    #말하신거 처럼 값이 이상하면 읽어오는 위치가 잘못 되었다고 생각 됩니다.
+    #값을 잘 못 읽어 들이는게 문제이면 try_item에서 handler를 호출할때에도 값이 이상해져야 하고
+    #list에서 처음에 읽어 들이는 handler도 이상해야 되는데 그건 아니니까요
 
 
 class EncodedCatchHandler(DexItem):
