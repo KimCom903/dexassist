3f77395547ed39d19c11ffc02ac9367fa0172995
KimCom903 <55732371+KimCom903@users.noreply.github.com>
2019-12-08 16:44:51 +0900

    Add files via upload

--- normalize.py
+++ normalize.py
@@ -119,16 +119,29 @@ class DexClassItem(object):
     OP_CONST_STRING = 0x1a
     ret.add(self.name)
     ret.add(self.type)
+    ret.add(self.superclass)
+    for ann in self.annotations:
+      ret.add(ann.type)
+      for ele in ann.elements:
+        ret.add(ele[0])
     for x in self.methods:
       editor = x.get_editor()
       for opcode in editor.opcodes:
         if opcode.op == OP_CONST_STRING:
           ret.add(opcode.BBBB)
+      for ann in x.annotations:
+        ret.add(ann.type)
+        for ele in ann.elements:
+          ret.add(ele[0])
       ret.add(x.shorty)
       ret.add(x.name)
       ret.add(x.return_type)
       ret.update(x.params)
     for x in self.fields:
+      for ann in x.annotations:
+        ret.add(ann.type)
+        for ele in ann.elements:
+          ret.add(ele[0])
       ret.add(x.type)
       ret.add(x.name)
     return ret
@@ -173,7 +186,6 @@ class DexMethod(object):
     self.access_flags = access_flags
     self.param_annotations = []
     self.annotation_set_ref_list_offset = NO_OFFSET
-    print('signature : {}'.format(self.signature))
     self.editor = editor
     self.code_item_offset = NO_OFFSET
   
@@ -238,7 +250,7 @@ class DexProto(object):
     self.return_type = return_type
     self.parameters = params
   def __hash__(self):
-    return hash(self.shorty)
+    return hash(self.return_type + "".join(self.parameters))
   def __eq__(self,othr):
     if self.shorty == othr.shorty:
       return True
