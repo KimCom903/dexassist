1e58a7431f2ac1069975c18fdf3f393e9d22aa4b
jsyoon <decinc.kr@gmail.com>
2019-12-14 10:48:24 +0900

    add apk test

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import zipfile, os
+
 from dexassist.dex import converter, dex
 from dexassist.writer import dex as writer_dex
 from dexassist.writer.dex.writer import DexWriter
@@ -30,13 +32,44 @@ def duplicate_dex(dex_path):
   p = DexWriter(mdex)
   p.write(stream)
 
+def remake(src, dst):
+  with zipfile.ZipFile(src, 'r') as s:
+    with zipfile.ZipFile(dst, 'w') as d:
+      for x in s.infolist():
+        if x.filename == 'classes.dex': continue
+        if 'META-INF' in x.filename: continue
+        buf = s.read(x.filename)
+        d.writestr(x, buf)
+
+def remake_apk(apk_path, out_apk_path):
+  with zipfile.ZipFile(apk_path, 'r') as f:
+    x = f.read('classes.dex')
+  
+  manager = dex.DexManager()
+  stream = dex.StreamReader(x, manager)
+  header = dex.HeaderItem(manager, stream, 0)
+  mdex = converter.DexConverter().get_dex(header, manager)
+  buf = bytearray()
+  stream = OutputStream(buf,0)
+  #mdex.write(stream)
+  p = DexWriter(mdex)
+  p.write(stream)
+  with open('test.dex', 'rb') as f:
+    buf = f.read()
+  remake(apk_path, out_apk_path)
+  
+
+  with zipfile.ZipFile(out_apk_path, 'a') as f:
+    
+    f.writestr('classes.dex', buf)
+  
 
 def main():
   #print_dex('test_binary/classes_mid.dex')
   #print_dex('test_binary/more_large.dex')
-  duplicate_dex('test_binary/classes_mid.dex')
+  #duplicate_dex('test_binary/classes_mid.dex')
   #duplicate_dex('test_binary/classes.dex')
   #duplicate_dex('test_binary/large.dex')
-
+  remake_apk('sample_apk/sample1.apk', 'out.apk')
 if __name__ == '__main__':
   main()
